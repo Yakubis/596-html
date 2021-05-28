27b9ede1c92683e98c832e619070f96a207f15a2
Yakubis <andru.yakubis@gmail.com>
2021-05-28 17:29:49 +0300

    Broken0.6

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+const newYearTime = new Date(`July 05 2021 00:00:00`);
 
 // Set background year
 
