10398acd46fd7e1a6b6032cf09ebe21583dba1f2
Yakubis <andru.yakubis@gmail.com>
2021-06-17 13:19:15 +0300

    Finnal0.33

--- js/products-sort.js
+++ js/products-sort.js
@@ -1,11 +1,12 @@
-
 let products;
 
 function renderTab(products, category) {
-    let html = '';
-    const productsByCategory = products.filter( product => product.category === category );
-    for (const product of productsByCategory) {
-      html += `
+  let html = "";
+  const productsByCategory = products.filter(
+    (product) => product.category === category
+  );
+  for (const product of productsByCategory) {
+    html += `
         <div class="shop-elements">
           <img src="img/${product.image}" alt="${product.title}" class="freshimg">
           <h2>${product.title}</h2>
@@ -13,23 +14,20 @@ function renderTab(products, category) {
           <button class="buybutton">Buy now</button>
         </div>
       `;
-    }
-    const productsContainer = document.querySelector('.shop-block');
-    productsContainer.innerHTML = html;
   }
+  const productsContainer = document.querySelector(".shop-block");
+  productsContainer.innerHTML = html;
+}
 
+const radio = document.getElementsByName("type");
 
-  const radio = document.getElementsByName('type');
+for (var i = 0; i < radio.length; i++) {
+  radio[i].onchange = testRadio;
+}
 
-  for (var i=0; i< radio.length; i++) {
-    radio[i].onchange = testRadio;
-  }
-  
-
-  function testRadio () {
-    renderTab(products, this.value);
-
-  } 
+function testRadio() {
+  renderTab(products, this.value);
+}
 
 // function fetchProdukts() {
 //     fetch('../js/products-list.json')
@@ -39,11 +37,9 @@ function renderTab(products, category) {
 // }
 
 async function fetchProdukts() {
-    const response = await fetch('products-list.json');
-    products = await response.json();
-    renderTab(products, 'fruit');
-    ;
+  const response = await fetch("products-list.json");
+  products = await response.json();
+  renderTab(products, "fruit");
 }
-
 
 fetchProdukts();
